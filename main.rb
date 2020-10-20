535f6bdf9240cb9a91296493fe45c919db2076dc
Jonas Arvidsson <jonas054@gmail.com>
2020-10-20 08:29:07 +0200

    Add support for A and D keys
    Same as left and right arrow keys.

--- main.rb
+++ main.rb
@@ -68,13 +68,15 @@ class BouncingBalls < Gosu::Window
 
   def button_down(id)
     case id
-    when Gosu::KB_LEFT, Gosu::KB_RIGHT then @pressed_key[id] = true
+    when Gosu::KB_LEFT, Gosu::KB_A then @pressed_key[Gosu::KB_LEFT] = true
+    when Gosu::KB_RIGHT, Gosu::KB_D then @pressed_key[Gosu::KB_RIGHT] = true
     end
   end
 
   def button_up(id)
     case id
-    when Gosu::KB_LEFT, Gosu::KB_RIGHT then @pressed_key[id] = nil
+    when Gosu::KB_LEFT, Gosu::KB_A then @pressed_key[Gosu::KB_LEFT] = nil
+    when Gosu::KB_RIGHT, Gosu::KB_D then @pressed_key[Gosu::KB_RIGHT] = nil
     end
   end
 
